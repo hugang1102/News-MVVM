ba88efb20f8805fd5d002657f869e7871377ee6c
hugang <626100057@qq.com>
2017-11-04 15:05:03 +0800

    添加工程

---
+++ News-MVVM/News-MVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News-MVVM
+//
+//  Created by dev on 2017/11/4.
+//  Copyright © 2017年 dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ News-MVVM/News-MVVM/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  News-MVVM
+//
+//  Created by dev on 2017/11/4.
+//  Copyright © 2017年 dev. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
